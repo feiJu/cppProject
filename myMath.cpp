7b14a17d891f5f197dd3abda6c4c952990adbf32
jufei <fenghun5987@gmail.com>
2017-06-09 15:29:11 +0800

    添加加减乘除功能

---
+++ myMath.cpp
@@ -0,0 +1,21 @@
+//
+// Created by jucf on 2017/6/9.
+//
+
+#include "myMath.h"
+
+int plus(int a,int b){
+    return a+b;
+}
+
+int multiplication(int a,int b){
+    return a*b;
+}
+
+int subtraction(int a,int b){
+    return a-b;
+}
+
+float division(float a,float b){
+    return a/b;
+}
---
+++ myMath.h
@@ -0,0 +1,37 @@
+//
+// Created by jucf on 2017/6/9.
+//
+
+#ifndef ASWORKSPACE_MYMATH_H
+#define ASWORKSPACE_MYMATH_H
+
+#endif //ASWORKSPACE_MYMATH_H
+
+/**
+ * 加法
+ * @param a
+ * @param b
+ * @return
+ */
+int plus(int a,int b);
+/**
+ * 乘法
+ * @param a
+ * @param b
+ * @return
+ */
+int multiplication(int a,int b);
+/**
+ * 减法
+ * @param a
+ * @param b
+ * @return
+ */
+int subtraction(int a,int b);
+/**
+ * 除法
+ * @param a
+ * @param b
+ * @return
+ */
+float division(float a,float b);
